9c6e555c9c2075f56296fb9640ba511fd4e68c41
Austin Hyun <57378337+syool@users.noreply.github.com>
2025-08-04 16:16:14 +0900

    Update controller.py

--- Blackboard/src/controller.py
+++ Blackboard/src/controller.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtWidgets import QApplication
 
 from src.model import Model
 from src.view import View
@@ -6,10 +7,9 @@ from src.view import View
 
 class Launcher(QObject):
     '''
-    실행 컨트롤러
+    the launch controller
         1. 이 컨트롤러는 parent 인자를 명시하지 않으면 기본적으로 Qbject를 상속받습니다.
         2. 이 컨트롤러를 실행하는 상위 컨트롤러의 self를 parent 인자로 상속받는 걸 권장합니다.
-        3. 이 컨트롤러가 앱 전체 최상위 컨트롤러일 경우, parent 인자를 명시하지 않아도 좋습니다.
     '''
     def __init__(
             self,
@@ -19,18 +19,20 @@ class Launcher(QObject):
         self.model = Model()
         self.view = View()
         self.controller = [
-            LabelController(self, self.model, self.view), # QLabel 기능 컨트롤러
-            ButtonController(self, self.model, self.view) # QPushButton 기능 컨트롤러
+            LineEditController(self, self.model, self.view),
+            LabelController(self, self.model, self.view),
+            ButtonController(self, self.model, self.view)
         ]
 
         self._do()
 
     def _do(self):
+        ''' controller functions '''
         self.view.show()
 
 
-class LabelController(QObject):
-    ''' QLabel 기능 컨트롤러 '''
+class LineEditController(QObject):
+    ''' a functional controller for QLineEdits '''
     def __init__(
             self,
             parent: Launcher,
@@ -45,16 +47,39 @@ class LabelController(QObject):
         self._do()
         
     def _do(self):
-        text = self.model.get_label_text_from_json()
-        self._on_label_updated(text)
-    
-    @pyqtSlot() 
-    def _on_label_updated(self, text):
-        self.view.my_label.setText(text)
+        ''' controller functions '''
+        self.view.line_input.textChanged.connect(self._get_input)
         
+    @pyqtSlot(str)
+    def _get_input(self, content: str) -> None:
+        self.model.input_content = content
+
+
+class LabelController(QObject):
+    ''' a functional controller for QLabels '''
+    def __init__(
+            self,
+            parent: Launcher,
+            model: Model,
+            view: View
+        ) -> None:
+        super().__init__(parent)
+        
+        self.model = model
+        self.view = view
+        
+        self._do()
+        
+    def _do(self):
+        self.model.userTextChanged.connect(self._set_label)
+        
+    @pyqtSlot(str)
+    def _set_label(self, content: str) -> None:
+        self.view.label_output.setText(content)
+
 
 class ButtonController(QObject):
-    ''' QPushButton 기능 컨트롤러 '''
+    ''' a functional controller for QPushButtons '''
     def __init__(
             self,
             parent: Launcher,
@@ -69,4 +94,14 @@ class ButtonController(QObject):
         self._do()
         
     def _do(self):
-        None
+        ''' controller functions '''
+        self.view.btn_apply.clicked.connect(self._on_apply_clicked)
+        self.view.btn_close.clicked.connect(self._on_close_clicked)
+        
+    @pyqtSlot()
+    def _on_apply_clicked(self) -> None:
+        self.model.text_json = self.model.input_content
+        
+    @pyqtSlot()
+    def _on_close_clicked(self) -> None:
+        QApplication.instance().quit()
