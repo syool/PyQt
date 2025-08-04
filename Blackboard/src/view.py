c0b3752308b6aff5d99016c4d4362ece5119540d
Austin Hyun <57378337+syool@users.noreply.github.com>
2025-08-04 17:57:13 +0900

    Update view.py

--- Blackboard/src/view.py
+++ Blackboard/src/view.py
@@ -3,6 +3,7 @@ from PyQt5 import uic
 widget_class, base_class = uic.loadUiType('./ui/test.ui')
 
 class View(base_class, widget_class):
+    ''' 뷰 '''
     def __init__(self):
         super().__init__()
         self.setupUi(self)
